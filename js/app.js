632dc474b2863acde948bde1e9960d4618c629a6
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-01 22:06:04 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 $(document).ready(function(){
-}
+	$(".add").on("click", function(){
+		var item = $("input[name=item]").val();
+		$("#shopping-list").append("<li>" + item + "</li>");
+		$("input[name=item]").val(""); 
+	})
+});     
+
+
+
+//1st - get text from input box and display it on page
+//2nd - delete an item after we've added it
+//3rd - check off an item after it has been added
