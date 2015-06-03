e94a5ef29947d7a2d47f6d0d9e91ac7bca6af61f
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-03 18:37:06 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function(){		
+	// 1st get text from input box and display it on page
 	$(".add").on("click", function(){
 		event.preventDefault(); //with this when you can also hit "enter" instead of clicking on the button all the time
 		var item = $("input[name=item]").val();
-		$("#shopping-list").append("<li>" + item + "<button>+</button><button>-</button></li>");
+		$("#shopping-list").append("<li>" + "<button class=addbutton>+</button>" + " " + "<button class=removebutton>-</button>" + " " + item + "</li>");
 		$("input[name=item]").val("");
 	})	
+	// 2nd delete an item after we've added it
+	$("<button class=removebutton>-</button>").click(function() {
+		$("#shopping-list").remove(item);
+	});
+
 }); 
 
 
 
-//1st - get text from input box and display it on page
-//2nd - delete an item after we've added it
-//3rd - check off an item after it has been added
+
+
+// 3rd - move item beneath "completed list" when the green
