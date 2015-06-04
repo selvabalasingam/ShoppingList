195d2d50efb7fcd40f0ee2a7c79b709dea58e032
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-04 12:48:01 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){        
-    // 1st get text from input box and display it on page
-    $(".add").on("click", function(){
-        event.preventDefault(); //with this when you can also hit "enter" instead of clicking on the button all the time
-        var item = $("input[name=item]").val();
-        $("#shopping-list").append("<li>" + "<button class=addbutton>+</button>" + " " + "<button class=removebutton>-</button>" + " " + item + "</li>");
-        $("input[name=item]").val("");
-    })    
-    // 2nd delete an item after we've added it
-    $("<button class=removebutton>-</button>").click(function() {
-        $("#shopping-list").remove(item);
-    });
-
-});
-
+$(document).ready(function(){		
+	// 1st get text from input box and display it on page
+	$(".add").on("click", function(){
+		event.preventDefault(); //with this when you can also hit "enter" instead of clicking on the button all the time
+		var item = $("input[name=item]").val();
+	if ("") {
+		alert ("You did not type your item")
+	} else {
+		$("#shopping-list").append("<li>" + "<button class=addbutton>+</button>" + " " + "<button class=removebutton>-</button>" + " " + item + "</li>");
+		$("input[name=item]").val("");
+	}
+	})	
+	// 2nd delete an item after we've added it
+	$("ul").on('click', 'button.removebutton', function() {$("#shopping-list").remove(item)});
+}); 
 
 //3rd - check off an item after it has been added
