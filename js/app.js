e07130486c447bea9aca4f3c20e774878de5b56f
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-04 16:04:12 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
 	$(".add").on("click", function(){
 		event.preventDefault(); //with this when you can also hit "enter" instead of clicking on the button all the time
 		var item = $("input[name=item]").val();
+	// If any the length of any item types is less or equal to zero you will get an alert message
 	if (item <= 0) {
 		alert ("You did not type your item")
+	// Otherwise, if you type in your list - it will appear!
 	} else {
 		$("#shopping-list").append("<li>" + "<button class=addbutton>+</button>" + " " + "<button class=removebutton>-</button>" + " " + item + "</li>");
 		$("input[name=item]").val("");
