588ccdd3451073b1c7d24165b66649577f5a0267
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-04 12:26:03 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
 	$(".add").on("click", function(){
 		event.preventDefault(); //with this when you can also hit "enter" instead of clicking on the button all the time
 		var item = $("input[name=item]").val();
+	if ("") {
+		alert ("You did not type your item")
+	} else {
 		$("#shopping-list").append("<li>" + "<button class=addbutton>+</button>" + " " + "<button class=removebutton>-</button>" + " " + item + "</li>");
 		$("input[name=item]").val("");
 	})	
+	}
 	// 2nd delete an item after we've added it
-	$("<button class=removebutton>-</button>").click(function() {
-		$("#shopping-list").remove(item);
+	$("ul").on('click', 'button.removebutton', function() {$("#shopping-list").remove(item)});
 	});
-
 }); 
 
 
